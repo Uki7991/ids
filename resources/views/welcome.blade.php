481e6686a062e29659564f99294c39893c2a61ac
uki7991 <tilek.kubanov@sibers.com>
2018-05-31 15:58:24 +0600

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -390,8 +390,8 @@
             responsive:{
                 0:{
                     items:1,
-                    nav:true,
-                    dots: true
+                    nav:false,
+                    dots: false
                 },
                 600:{
                     items:2,
