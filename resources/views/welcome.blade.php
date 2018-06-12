7187af7fc88969db6455463f0e3ed0c33d20bbd3
Uki7991 <tilek.kubanov@gmail.com>
2018-06-12 17:26:30 +0600

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Стоматологическая клиника')
+@section('title', 'Стоматологическая клиника в бишкеке. Профессионалы высшего класса')
 
 @section('content')
 
